e728ca4af3b631e7fadc06c49f2514644a6d1865
Sunmeet Singh <renu.kabeer@gmail.com>
2022-12-22 21:10:32 +0530

    backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { AppService } from './app.service';
 import { WisdomModule } from './wisdom-apis/wisdom.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RecruiterModule } from './recruiterflow-apis/recruiter.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -14,9 +13,6 @@ import { ConfigModule } from '@nestjs/config';
       useFactory: () => ({
         uri: 'mongodb+srv://sunmeet:sunmeet@cluster0.zqxxfuf.mongodb.net/?retryWrites=true&w=majority',
       }),
-    }),
-    ConfigModule.forRoot({
-      isGlobal: true,
     }),
   ],
   controllers: [AppController],
