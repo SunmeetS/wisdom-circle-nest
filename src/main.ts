7b6b34d3ab79e33ea1dc93cf3000bf431ba7e643
Sunmeet Singh <renu.kabeer@gmail.com>
2023-03-07 23:55:50 +0530

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-
+  const config = new DocumentBuilder()
+    .setTitle('All APIs')
+    .setDescription('All the required APIs in one server.')
+    .setVersion('1.0')
+    .addTag('swagger')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
